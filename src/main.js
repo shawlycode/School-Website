2bed7edab5768092291be969e1249b0c16c76342
Shawlycode <kutinaorlando@gmail.com>
2023-02-20 18:06:55 +0000

    added swiper js

--- src/main.js
+++ src/main.js
@@ -3,3 +3,18 @@ window.addEventListener("scroll", () => {
     .querySelector("nav")
     .classList.toggle("window-scroll", window.scrollY > 100);
 });
+// show/hide faqs
+
+const faqs = document.querySelectorAll(".faq");
+faqs.forEach((faq) => {
+  faq.addEventListener("click", () => {
+    faq.classList.toggle("open");
+
+    const icon = faq.querySelector(".faq__icon i");
+    if (icon.className === "uil uil-plus") {
+      icon.className = "uil uil-minus";
+    } else {
+      icon.className = "uil uil-plus";
+    }
+  });
+});
